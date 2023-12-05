9155dd2ca8b2a5f094c9908b1330dd377130e657
Hussam <hussam@dominantstrategies.io>
2023-12-05 14:15:59 -0600

    Temp add bootnode

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -6,5 +6,6 @@ var (
 		// "/ip4/35.223.198.14/tcp/4001/p2p/12D3KooWDWPz7ghZzBXfygv5Q62ijk3XFZYYjgSXfQkXgQPBGtA8",
 		// "/ip4/34.31.144.9/tcp/4001/p2p/12D3KooWAsr23smoj51VxUrLM7LvjSjCvwzarB6DMaa5eAcs9qeH",
 		"/ip4/35.226.76.0/tcp/4001/p2p/12D3KooWHsJ73d4G9gKtL7VnBfSQBWeSEnpBaDFvr7QiX1JoeGjB",
+		"/ip4/34.31.180.127/tcp/4001/p2p/12D3KooWPVv6PjBwkNho1qR8TH5K3kt4TGVqAg2P2PVuav9rakrE",
 	}
 )
