fc96c90c16e4661baa690c1adf3449327dc471d2
Hussam <hussam@dominantstrategies.io>
2023-12-06 15:50:45 -0600

    Change bootnodes

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -2,8 +2,9 @@ package common
 
 var (
 	BootstrapPeers = []string{
-		"/ip4/35.226.76.0/tcp/4001/p2p/12D3KooWBS6iiRnokwdDbrJRNfxAmpUPhZF9Ns8ofoh7WrYMUaTk",
-		"/ip4/34.68.124.139/tcp/4001/p2p/12D3KooWJr13DAjGkrstCEpYG2F5F15rCugwtyp5G1VLe1gxSDja",
-		"/ip4/34.41.131.24/tcp/4001/p2p/12D3KooWE2fcnqsTwUpe72C7ryYqsBds1KVqwd3Jk6besbuyFg3j",
+		"/ip4/35.226.76.0/tcp/4001/p2p/12D3KooWHsJ73d4G9gKtL7VnBfSQBWeSEnpBaDFvr7QiX1JoeGjB",
+		"/ip4/34.31.180.127/tcp/4001/p2p/12D3KooWJ8d3AnGzscdVf2kzxzTbJgDtDXqrHS8vnedrFmZr8uvK",
+		"/ip4/34.68.124.139/tcp/4001/p2p/12D3KooWLxXHZx2QGjJra2oWjGYQ3Vdrkew5cNZcEgfaiKi7Dfrx",
+		"/ip4/34.41.131.24/tcp/4001/p2p/12D3KooWD8kycTRgZAovnciaoyNzcJxW16hCFNSfJYm4Lj6ouCBd",
 	}
 )
