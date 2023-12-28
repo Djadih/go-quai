05b2455276c909fa7175714a4e7f0b1307dabf60
Alejo Acosta <alejoacos@gmail.com>
2023-12-28 15:03:01 -0300

    add function to convert string into Hash type

--- consensus/types/types.go
+++ consensus/types/types.go
@@ -1,11 +1,26 @@
 package types
 
 import (
+	"encoding/hex"
 	"strconv"
+
+	"github.com/pkg/errors"
 )
 
 // Hash represents a 256 bit hash
 type Hash [32]byte
+
+func NewHashFromString(s string) (Hash, error) {
+	if len(s) != 64 { // 2 hex chars per byte
+		return Hash{}, errors.New("invalid string length for hash")
+	}
+	var h Hash
+	_, err := hex.Decode(h[:], []byte(s))
+	if err != nil {
+		return Hash{}, err
+	}
+	return h, nil
+}
 
 type Context struct {
 	location string
