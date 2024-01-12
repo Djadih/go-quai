ff59caf29e5c22daac0e151fe940679fae237117
gop <gameofpointers@proton.me>
2024-01-12 14:15:15 -0600

    Implemented the Initial BroadcastBlock logic from core side

--- p2p/node/api.go
+++ p2p/node/api.go
@@ -57,12 +57,14 @@ func (p *P2PNode) Start() error {
 	return nil
 }
 
-func (p *P2PNode) Subscribe(slice types.SliceID, data interface{}) error {
-	return p.pubsub.Subscribe(slice, data)
+func (p *P2PNode) Subscribe(data interface{}, location common.Location) error {
+	log.Warn("Subscribed to Blocks", "location", location)
+	return p.pubsub.Subscribe(LocationToSliceID(location), data)
 }
 
-func (p *P2PNode) Broadcast(slice types.SliceID, data interface{}) error {
-	return p.pubsub.Broadcast(slice, data)
+func (p *P2PNode) Broadcast(data interface{}, location common.Location) error {
+	log.Warn("Broadcast Block", "block", data)
+	return p.pubsub.Broadcast(LocationToSliceID(location), data)
 }
 
 func (p *P2PNode) SetConsensusBackend(be quai.ConsensusAPI) {
@@ -107,6 +109,28 @@ func (p *P2PNode) Stop() error {
 	} else {
 		return nil
 	}
+}
+
+func LocationToSliceID(location common.Location) types.SliceID {
+	var context types.Context
+	var sliceID types.SliceID
+	switch location.Context() {
+	case common.PRIME_CTX:
+		context = types.PRIME_CTX
+	case common.REGION_CTX:
+		context = types.REGION_CTX
+		sliceID.Region = uint32(location.Region())
+	case common.ZONE_CTX:
+		context = types.ZONE_CTX
+		sliceID.Region = uint32(location.Region())
+		sliceID.Zone = uint32(location.Zone())
+	}
+	sliceID.Context = context
+	return sliceID
+}
+
+func (p *P2PNode) BroadcastTransaction(tx types.Transaction) error {
+	panic("todo")
 }
 
 // Request a block from the network for the specified slice
