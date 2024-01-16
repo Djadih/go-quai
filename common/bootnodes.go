049e7eb4a8cd13f719120fba46f2404ce31b553b
Hussam <hussam@dominantstrategies.io>
2024-01-16 17:42:37 -0600

    Local bootnode

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -2,6 +2,6 @@ package common
 
 var (
 	BootstrapPeers = []string{
-		"/ip4/72.182.96.176/tcp/4001/p2p/12D3KooWJVb6EkVAyx7uCGaYzDtV7Hv9ufUdTphXxw99Huh9nKRs",
+		"/ip4/192.168.4.7/tcp/4001/p2p/12D3KooWJVb6EkVAyx7uCGaYzDtV7Hv9ufUdTphXxw99Huh9nKRs",
 	}
 )
